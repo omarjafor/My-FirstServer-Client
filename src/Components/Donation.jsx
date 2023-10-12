c13fa1526dc27342c399c2f5f92881ef553b363c
omarjafor <omarjaforchy@gmail.com>
2023-10-12 16:11:19 +0600

    use effect problem solved

--- src/Components/Donation.jsx
+++ src/Components/Donation.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 
 const Donation = () => {
+
+
     const [donations, setDonations] = useState([]);
 
     useEffect(() => {
@@ -10,20 +12,19 @@ const Donation = () => {
             .then(res => res.json())
             .then(data => setDonations(data))
     }, [])
-    
-    console.log(donations);
 
+    // const donations = useLoaderData();
     const { id } = useParams();
     console.log(id);
 
-    const donation = donations.find(donat => donat.id === parseInt(id));
+    const donation = donations?.find(donat => donat?.id === parseInt(id));
     console.log(donation);
 
     return (
         <div>
-            {/* <h4 className="text-2xl font-bold"> {donation.id} </h4>
-            <h1 className="text-4xl font-bold mb-4"> {donation.title} </h1>
-            <img className="rounded-xl" src={donation.picture} alt="" /> */}
+            <h4 className="text-2xl font-bold"> {donation?.id} </h4>
+            <h1 className="text-4xl font-bold mb-4"> {donation?.title} </h1>
+            <img className="rounded-xl" src={donation?.picture} alt="" />
         </div>
     );
 };
